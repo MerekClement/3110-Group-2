8c49376452a766575affb1f3ff68c520abc9f418
Kathan Patel <kathanpatel@cmail.carleton.ca>
2022-11-09 22:26:34 -0500

    Added pass and clear buttons, and resized the window to fit everything

--- src/BoardController.java
+++ src/BoardController.java
@@ -2,8 +2,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BoardController implements ActionListener {
+    BoardFrame frame;
+    BoardManager manager;
     public BoardController(BoardFrame boardFrame) {
-
+        frame = boardFrame;
+        manager = frame.boardManager;
     }
 
     @Override
--- src/BoardFrame.java
+++ src/BoardFrame.java
@@ -5,9 +5,11 @@ import java.awt.*;
 
 public class BoardFrame extends JFrame implements BoardView {
     private JButton[][] cells = new JButton[15][15];
+
+    private JButton passPlayer1btn, passPlayer2btn, clearPlayer1, clearPlayer2;
     private BoardController boardController;
 
-    private BoardManager boardManager = new BoardManager(new Utils(false));
+    BoardManager boardManager = new BoardManager(new Utils(false));
     private JPanel bagOfPlayer1, bagOfPlayer2, boardPanel;
 
     private JButton[] alphabetButtonsPlayer1, alphabetButtonsPlayer2;
@@ -19,12 +21,12 @@ public class BoardFrame extends JFrame implements BoardView {
         GridBagConstraints c = new GridBagConstraints();
         setLayout(gb);
 
-        bagOfPlayer1 = new JPanel(new GridLayout(8,1));
+        bagOfPlayer1 = new JPanel(new GridLayout(10,1));
         boardPanel  = new JPanel(new GridLayout(16,16,3,3));
-        bagOfPlayer2 = new JPanel(new GridLayout(8,1));
+        bagOfPlayer2 = new JPanel(new GridLayout(10,1));
 
-        bagOfPlayer1.add(new JLabel("Player 1"));
-        bagOfPlayer2.add(new JLabel("Player 2"));
+        bagOfPlayer1.add(new JLabel("Player 1",SwingConstants.CENTER));
+        bagOfPlayer2.add(new JLabel("Player 2",SwingConstants.CENTER));
 
         alphabetButtonsPlayer1 = new JButton[7];
         alphabetButtonsPlayer2 = new JButton[7];
@@ -34,19 +36,19 @@ public class BoardFrame extends JFrame implements BoardView {
 
         for(int i = 0; i < 16; i++){
             if(i>0) {
-                boardPanel.add(new JLabel((char) (a + i - 1) + ""));
+                boardPanel.add(new JLabel( ( i ) + "",SwingConstants.CENTER));
             }else{
-                boardPanel.add(new JLabel("R/C"));
+                boardPanel.add(new JLabel("  ",SwingConstants.CENTER));
             }
             for(int j = 1; j < 16; j++){
                 if(i == 0){
-                    boardPanel.add(new JLabel((char)(a+j-1) + ""));
+                    boardPanel.add(new JLabel((char)(a+j-1) + "",SwingConstants.CENTER));
                     continue;
                 }
                 cells[i-1][j-1] = new JButton(" ");
                 cells[i-1][j-1].setActionCommand(i +""+ j);
                 cells[i-1][j-1].addActionListener(boardController);
-                boardPanel.add(cells[i-1][j-1]);
+                boardPanel.add(cells[i-1][j-1], CENTER_ALIGNMENT);
             }
         }
         String player1Alphabet, player2Alphabet;
@@ -65,7 +67,18 @@ public class BoardFrame extends JFrame implements BoardView {
             alphabetButtonsPlayer2[i].setActionCommand(player2Alphabet);
         }
 
-
+        passPlayer1btn = new JButton("Pass");
+        passPlayer2btn = new JButton("Pass");
+        clearPlayer1 = new JButton("Clear");
+        clearPlayer2 = new JButton("Clear");
+        bagOfPlayer1.add(passPlayer1btn);
+        bagOfPlayer2.add(passPlayer2btn);
+        bagOfPlayer1.add(clearPlayer1);
+        bagOfPlayer2.add(clearPlayer2);
+        passPlayer1btn.addActionListener(boardController);
+        passPlayer2btn.addActionListener(boardController);
+        clearPlayer2.addActionListener(boardController);
+        clearPlayer1.addActionListener(boardController);
 
         JLabel label = new JLabel("S C R A B B L E");
 
@@ -95,7 +108,7 @@ public class BoardFrame extends JFrame implements BoardView {
         c.gridheight = 1;
         add(bagOfPlayer2, c);
 
-        setSize(new Dimension(950,630));
+        setSize(new Dimension(975,630));
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
